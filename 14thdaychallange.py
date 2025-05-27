dfa96e12aacc5869fe46d87b5fd45c5f449aa837
nishchya08 <nishchya786@gmail.com>
2025-05-27 23:42:11 +0530

    Adding python challnage files

--- 14thdaychallange.py
+++ 14thdaychallange.py
@@ -7,15 +7,32 @@ while n<4:
     if n==3:
         continue
     print(n)
+
 print("-----------------------------------------")
 
 func=[]
 for i in range(5):
     func.append(lambda x=i: x)
 print([f() for f in func])
+
 print("------------------------------")
 
 for i in range(1,10,3):
     print(i)
     if(i==4):
         break
+print("-----------------------")
+
+data=[(1,3),(3,1),(2,3)]
+sorted_data=sorted(data, key=lambda x:x[1])
+print(sorted_data)
+
+
+print("---------------")
+
+
+from collections import deque
+dq= deque([1,2,3])
+dq.append(4)
+print(dq)
+
