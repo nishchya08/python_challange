6e5b0beb6ed5d142c39b411a44e0ab623f319c60
nishchya08 <nishchya786@gmail.com>
2025-06-11 07:48:25 +0530

    Adding python challnage files

---
+++ 27thdaychallange.py
@@ -0,0 +1,22 @@
+x=3.14
+y=-4
+z=5
+#print(round(x))
+#print(abs(y))
+#result=max(x,y,z)
+#result=min(x,y,z)
+'''
+import math
+#print(math.pi)
+#print(math.e)
+
+radius=float(input("Enter radius: "))
+circumference=2*math.pi*radius
+print(f"The circumference is {round(circumference,2)} ")
+'''
+
+import math
+a=float(input("Enter side A: "))
+b=float(input("Enter side B: "))
+c =math.sqrt(pow(a,2) + pow(b,2))
+print(f"Side C {pow(c, 2)}")
