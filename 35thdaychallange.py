d954b859b7d3e7bcbd170333039aa5a814962412
nishchya08 <nishchya786@gmail.com>
2025-06-23 23:49:27 +0530

    Adding python challnage files

---
+++ 35thdaychallange.py
@@ -0,0 +1,17 @@
+
+"""
+age=int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are now signed up!")
+elif age <= 0:
+    print("You have not been born yet!")
+else:
+    print("You must be 18+ to sign up")
+
+"""
+for_sale = True
+if for_sale:
+    print("The user is online")
+else:
+    print("The user is offline")
