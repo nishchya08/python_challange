8bbb8c6d3171f19176340f95bda5200c99bbdfad
nishchya08 <nishchya786@gmail.com>
2025-06-26 07:02:43 +0530

    Adding python challnage files

---
+++ 40thdaychallange.py
@@ -0,0 +1,10 @@
+#String method in Python are easy
+
+name = input("What is your name? ")
+#result =len(name)
+#result = name.find("h")
+#result = name.rfind("K")
+#name = name.upper()
+name = name.lower()
+print(name)
+#print(result)
