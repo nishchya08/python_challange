6bee6bc3168488d046b9d0ceb14ede28b27e8c85
nishchya08 <nishchya786@gmail.com>
2025-06-28 06:56:53 +0530

    Adding python challnage files

---
+++ 48thdaychallange.py
@@ -0,0 +1,22 @@
+# collections = single "variable" use to store multiple values
+#list = [] ordered and changeable . Duplicates OK
+# set = {} unordered and immutable , But Add/Remove OK. No duplicates
+# Tuple = () unordered and unchanagable . Duplicates Ok . Ffaster
+
+#Fruits = ["Mango", "Orange", "Banana"]
+#print(Fruits[::-1])
+#for fruit in Fruits:
+#     print(fruit, end = " ")
+#print("lichi" in Fruits)
+#Fruits.remove("Mango")
+#Fruits.append("Mango")
+#Fruits.insert(0,"Orange")
+#Fruits.clear()
+#print(Fruits.index("Mango"))
+#print(Fruits.count("Mango"))
+
+fruits = {"Mango", "Orange", "Banana"}
+fruits.pop()
+print(fruits)
+
+
