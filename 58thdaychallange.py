548ccc9697a674d69e851133891f401bae08389f
nishchya08 <nishchya786@gmail.com>
2025-06-29 17:21:41 +0530

    Adding python challnage files

---
+++ 58thdaychallange.py
@@ -0,0 +1,49 @@
+import random
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+number_of_dice = int(input("How many dice! "))
+
+for die in range(number_of_dice):
+    dice.append(random.randint(1, 6))
+#print(dice)
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end= "")
+    print()
+
+for die in dice:
+    total+=die
+print(f"The total is: {total}")
