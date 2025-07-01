64963bf0f13508dd773314d4baf8b619c82587f5
nishchya08 <nishchya786@gmail.com>
2025-07-01 07:19:25 +0530

    Adding python challnage files

---
+++ 65thdaychallange.py
@@ -0,0 +1,27 @@
+# Membership operators
+
+# word = "APPLE"
+# letter = input("Guess a letter in the secret word: ")
+
+# if letter in word:
+#    print(f"{letter} is in the word.")
+# else:
+#    print(f"{letter} is not in the word.")
+
+# grades = {"Sandy": "A",
+#          "Bobby": "B",
+#          "tinkus": "C",
+#          "dokki": "D"}
+# student = input("Enter the name of a student: ")
+
+# if student in grades:
+#     print(f"{student}'s grade is {grades[student]}")
+# else:
+#     print(f"{student} is not found")
+
+email = "nishchya786gmail.com"
+
+if "@" in email and "." in email:
+    print("Valid email")
+else:
+    print("Invalid email")
