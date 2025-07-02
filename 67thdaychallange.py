f97754da2e63fe654057f3f4652bf7d3456f1065
nishchya08 <nishchya786@gmail.com>
2025-07-02 07:46:01 +0530

    Adding python challnage files

---
+++ 67thdaychallange.py
@@ -0,0 +1,12 @@
+# Match_case
+
+def day_of_the_week(day):
+    match day:
+      case "Saturday" | "Sunday":
+        return True
+      case "Tuesay" | "Wednesday" | "Thursday" | "Friday" | "Monday":
+        return "False"
+      case _:
+        return False
+
+print(day_of_the_week("Sunday"))
