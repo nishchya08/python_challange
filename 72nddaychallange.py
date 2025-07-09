748bc218844a5cc7e87689fa96eada171d1ee6cf
nishchya08 <nishchya786@gmail.com>
2025-07-09 06:15:32 +0530

    Adding python challnage files

--- 72nddaychallange.py
+++ 72nddaychallange.py
@@ -24,15 +24,18 @@ hangman_art = {0: ("   ",
                                    "/ \\")}
 
 words = ("aardvark", "alligator", "alpaca", "ant", "anteater", "antelope", "ape", "armadillo", "baboon", "badger", "bat", "bear", "beaver", "bee", "bison", "boar", "buffalo", "butterfly", "camel", "capybara", "caribou", "cat", "caterpillar", "cattle", "chamois", "cheetah", "chicken", "chimpanzee", "chinchilla", "chough", "clam", "cobra", "cockroach", "cod", "coyote", "crab", "crane", "crocodile", "crow", "curlew", "deer", "dinosaur", "dog", "dogfish", "dolphin", "donkey", "dormouse", "dotterel", "dove", "dragonfly", "duck", "dugong", "dunlin", "eagle", "echidna", "eel", "eland", "elephant",  "elk", "emu", "falcon", "ferret", "finch", "fish", "flamingo", "fly", "fox", "frog", "gaur", "gazelle", "gerbil", "giraffe", "gnat", "gnu", "goat", "goldfinch", "goldfish", "goose", "gorilla", "goshawk", "grasshopper", "grouse", "guanaco", "gull", "hamster", "hare", "hawk", "hedgehog", "heron", "herring", "hippopotamus", "hornet", "horse", "human", "hummingbird", "hyena", "ibex", "ibis", "jackal", "jaguar", "jay", "jellyfish", "kangaroo", "kingfisher", "koala", "kookabura", "kouprey", "kudu", "lapwing", "lark", "lemur", "leopard", "lion", "llama", "lobster", "locust", "loris", "louse", "lyrebird", "magpie", "mallard", "manatee", "mandrill", "mantis", "marten", "meerkat", "mink", "mole", "mongoose", "monkey", "moose", "mosquito", "mouse", "mule", "narwhal", "newt", "nightingale", "octopus", "okapi", "opossum", "oryx", "ostrich", "otter", "owl", "ox", "oyster", "panda", "panther", "parrot", "partridge", "peafowl", "pelican", "penguin", "pheasant", "pig", "pigeon", "polar-bear", "pony", "porcupine", "porpoise", "quail", "quelea", "quetzal", "rabbit", "raccoon", "rail", "ram", "rat", "raven", "red-deer", "red-panda", "reindeer", "rhinoceros", "rook", "salamander", "salmon", "sand-dollar", "sandpiper", "sardine", "scorpion", "seahorse", "seal", "shark", "sheep", "shrew", "skunk", "snail", "snake", "sparrow", "spider", "spoonbill", "squid", "squirrel", "starling", "stingray", "stoat", "stork", "swallow", "swan", "tapir", "tarsier", "termite", "tiger", "toad", "trout", "turkey", "turtle", "viper", "vulture", "wallaby", "walrus", "wasp", "weasel", "whale", "wildcat", "wolf", "wolverine", "wombat", "woodcock", "woodpecker", "worm", "wren", "yak", "zebra")
-def display_man(wrong_guess):
-    for line in hangman_art[wrong_guess]:
+
+def display_man(wrong_guesses):
+    print("**********")
+    for line in hangman_art[wrong_guesses]:
         print(line)
+    print("**********")
 
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 def main():
     answer = random.choice(words)
@@ -40,18 +43,21 @@ def main():
     wrong_guesses = 0
     guessed_letters = set()
     is_running = True
+
     while is_running:
         display_man(wrong_guesses)
         display_hint(hint)
-        guess = input("Guess a letter: ").lower()
+        guess = input("Enter a letter: ").lower()
+
         if len(guess) != 1 or not guess.isalpha():
             print("Invalid input")
             continue
-        if guess in guessed_letters:
-            print(f"{guess} was already guessed")
-            continue
-        guessed_letters.add(guess)
 
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+
+        guessed_letters.add(guess)
 
         if guess in answer:
             for i in range(len(answer)):
@@ -65,10 +71,11 @@ def main():
             display_answer(answer)
             print("YOU WIN!")
             is_running = False
-
-
-
-
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE!")
+            is_running = False
 
 if __name__ == "__main__":
     main()
