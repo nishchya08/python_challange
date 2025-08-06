9828d891c844789b25d294ab6a581330f6e7f15e
nishchya08 <nishchya786@gmail.com>
2025-08-06 07:09:30 +0530

    Adding python challnage files

---
+++ 84thdaychallange.py
@@ -0,0 +1,12 @@
+# SORTING in PYTHON .sort() or sorted()
+# Lists[], Tuples(), Dictionaries{"":""}, Objects
+
+
+fruits =('Apple', 'Mango', 'Orange', 'Banana')
+#fruits = [2,43,2,2,2,4,4]
+#fruits.sort(reverse=True)
+fruits = tuple(sorted(fruits, reverse=True))
+print(fruits)
+
+
+
