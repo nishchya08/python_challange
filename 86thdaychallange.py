a7b0ec4cb9bdd060934f3b707999df89575db69c
Nishchya Kataria <nishchyakataria@Nishchyas-MacBook-Air.local>
2025-08-11 07:28:27 +0530

    Add 86th day challange script

---
+++ 86thdaychallange.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#array = np.array([1,2,3,])
+#print(array + 1)
+#print(array - 1)
+#print(array - 2)
+#print(array * 4)
+#print(array / 4)
+
+array = np.array([1.01, 2.5, 3.99])
+print(np.sqrt(array))  # Square root of each element
+print(np.ceil(array))  # Round each element to the nearest integer
