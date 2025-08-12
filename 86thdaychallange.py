2dc7cbc2bce48efd2a0d007a95f8badf3ac29b79
Nishchya Kataria <nishchyakataria@Nishchyas-MacBook-Air.local>
2025-08-12 06:50:21 +0530

    Update 86thdaychallange.py

--- 86thdaychallange.py
+++ 86thdaychallange.py
@@ -7,6 +7,20 @@ import numpy as np
 #print(array * 4)
 #print(array / 4)
 
-array = np.array([1.01, 2.5, 3.99])
-print(np.sqrt(array))  # Square root of each element
-print(np.ceil(array))  # Round each element to the nearest integer
+#array = np.array([1.01, 2.5, 3.99])
+#print(np.sqrt(array))  # Square root of each element
+#print(np.ceil(array))  # Round each element to the nearest integer
+
+#radii = np.array([1,2,3])
+#print(np.pi * radii ** 2)
+
+#array1 = np.array([1,2,3])
+#array2 = np.array([4,5,6])
+#print(array1 + array2)
+#print(array1 - array2)
+#print(array1 * array2)
+#print(array1 / array2)
+#print(array1 ** array2)  # Element-wise exponentiation
+
+scores = np.array([55,34,23,76,333,975,59])
+print(scores> 60)
