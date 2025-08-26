00bc63f2eab2517e8b462411b42c173affd89770
Nishchya Kataria <nishchyakataria@Nishchyas-MacBook-Air.local>
2025-08-26 07:32:22 +0530

    Adding 98thdaychallange

--- 98thdaychallanege.py
+++ 98thdaychallanege.py
@@ -1,7 +1,14 @@
 import os
 
-file_path = "stuff/test.txt"
+#file_path = "stuff/test.txt"
+file_path = "/Users/nishchyakataria/Projects/python_challange/stuff/test.txt"
 if os.path.exists(file_path):
     print(f"The location {file_path} exists.")
+    if os.path.isfile(file_path):
+        print(f"The file {file_path} is a file.")
+    else:
+        print(f"The location {file_path} is not a file.")
 else:
     print(f"The location {file_path} does not exist.")
+
+    
